98e7ec05a103df3660de9752a11b45c7faa8a970
FabPch <fabien191@hotmail.com>
2023-02-28 22:22:35 +0100

    write answer to Excel file, update condition

--- quartiers-prioritaires.py
+++ quartiers-prioritaires.py
@@ -1,58 +1,60 @@
-import csv
 import requests
 import pandas as pd
 
 POST_URL = 'https://sig.ville.gouv.fr/recherche-adresses-qp-polville-v2019'
 PRIORITAIRE = 'adresse recherchée est située dans le quartier prioritaire'
 NON_PRIORITAIRE = 'est pas située dans un quartier prioritaire'
+NON_PRIORITAIRE_2 = 'abrite pas de quartiers prioritaires'
+NON_PRIORITAIRE_3 = 'pas de lien avec un quartier prioritaire'
 NON_TROUVE = 'Aucune voie correspondante pour cette recherche'
+NON_TROUVE_2 = 'Veuillez précisez le numéro recherché dans la voie'
+FILE_NAME = 'organizations-11557216-596.xlsx'
 
 def get_adresse_info(adresse):
     payload = {
-        'num_adresse': adresse['num_adresse'],
-        'nom_voie': adresse['nom_voie'],
-        'nom_commune': adresse['nom_commune'],
-        'code_postal': adresse['code_postal'],
+        'num_adresse': adresse['Organisation - Numéro de maison'],
+        'nom_voie': adresse['Organisation - Nom de rue/route'],
+        'nom_commune': adresse['Organisation - Ville/agglomération/village/localité'],
+        'code_postal': adresse['Organisation - Code postal'],
     }
     response = requests.post(POST_URL, data=payload)
+    response.raise_for_status()
     response.encoding = 'utf-8'
     return response.json()
 
-def print_adresse_info(adresse_info, adresse):
-    if PRIORITAIRE in adresse_info['fullResponseTpl']:
-        print('zone prioritaire pour', adresse['nom_commune'])
-    elif NON_PRIORITAIRE in adresse_info['fullResponseTpl']:
-        print('zone non prioritaire pour', adresse['nom_commune'])
-    elif NON_TROUVE in adresse_info['fullResponseTpl']:
-        print('non trouvé pour', adresse['nom_commune'])
+def is_in_quartier_prioritaire(adresse_info, adresse):
+    info = adresse_info['fullResponseTpl']
+    nom_commune = adresse['Organisation - Ville/agglomération/village/localité']
+
+    if PRIORITAIRE in info:
+        return 'True'
+    elif (NON_PRIORITAIRE in info) | (NON_PRIORITAIRE_2 in info) | (NON_PRIORITAIRE_3 in info):
+        return 'False'
+    elif (NON_TROUVE in info) | (NON_TROUVE_2 in info):
+        return 'Non trouvé'
     else:
-        print('autre chose pour', adresse['nom_commune'])
+        print('autre chose pour', nom_commune)
+        # print(adresse_info['fullResponseTpl'])
+        return 'Presque...'
 
 def main():
-    with open('adresses.csv', encoding='utf-8') as adresses_file:
-        reader = csv.DictReader(adresses_file, delimiter=',')
-        for adresse in reader:
+    df = pd.read_excel(FILE_NAME, dtype=str)
+
+    print(f'row count: {len(df.index)}')
+
+    for index, adresse in df.iterrows():
+        try:
             adresse_info = get_adresse_info(adresse)
-            print_adresse_info(adresse_info, adresse)
+            result = is_in_quartier_prioritaire(adresse_info, adresse)
+            df.loc[index, 'is_in_quartier_prioritaire'] = result
+        except Exception as err:
+            print(err)
+            df.loc[index, 'is_in_quartier_prioritaire'] = 'Non trouvé'
+    
+    num_non_trouvées = (df['is_in_quartier_prioritaire'] == 'Non trouvé').sum()
+    print(f'Nombre adresses non trouvées: {num_non_trouvées}')
 
-# if __name__ == '__main__':
-#     main()
+    df.to_excel('response.xlsx', index=False)
 
-def read_excel_file(file_name):
-    use_cols = ['Organisation - Numéro de maison', 'Organisation - Nom de rue/route', 'Organisation - Ville/agglomération/village/localité', 'Organisation - Code postal']
-
-    df = pd.read_excel(file_name, usecols=use_cols, dtype=str)
-
-    new_col_names =  {
-        use_cols[0]: 'num_adresse'
-        , use_cols[1]: 'nom_voie'
-        , use_cols[2]: 'nom_commune'
-        , use_cols[3]: 'code_postal'
-    }
-
-    df_renamed = df.rename(mapper=new_col_names, axis=1)
-
-    df_with_json_payload = df_renamed.apply(lambda x: x.to_json(), axis=1)
-    print(df_with_json_payload.head(10))
-
-read_excel_file('organizations-11557216-596.xlsx')
+if __name__ == '__main__':
+    main()
