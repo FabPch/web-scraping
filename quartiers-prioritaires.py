c1ce37eac987067fc3e58bd15f6129bf3351ed6a
FabPch <fabien191@hotmail.com>
2023-03-01 22:33:08 +0100

    Add messages to expect from web response

--- quartiers-prioritaires.py
+++ quartiers-prioritaires.py
@@ -2,13 +2,19 @@ import requests
 import pandas as pd
 
 POST_URL = 'https://sig.ville.gouv.fr/recherche-adresses-qp-polville-v2019'
+
 PRIORITAIRE = 'adresse recherchée est située dans le quartier prioritaire'
 NON_PRIORITAIRE = 'est pas située dans un quartier prioritaire'
 NON_PRIORITAIRE_2 = 'abrite pas de quartiers prioritaires'
 NON_PRIORITAIRE_3 = 'pas de lien avec un quartier prioritaire'
+NON_PRIORITAIRE_4 = 'adresse recherchée est située dans la ZFU'
 NON_TROUVE = 'Aucune voie correspondante pour cette recherche'
 NON_TROUVE_2 = 'Veuillez précisez le numéro recherché dans la voie'
+NON_TROUVE_3 = 'Choisissez une commune parmi la liste'
+NON_TROUVE_4 = 'Nous ne sommes pas en mesure de pr'
+
 FILE_NAME = 'organizations-11557216-596.xlsx'
+FILE_NAME_ANSWER = 'response.xlsx'
 
 def get_adresse_info(adresse):
     payload = {
@@ -28,13 +34,14 @@ def is_in_quartier_prioritaire(adresse_info, adresse):
 
     if PRIORITAIRE in info:
         return 'True'
-    elif (NON_PRIORITAIRE in info) | (NON_PRIORITAIRE_2 in info) | (NON_PRIORITAIRE_3 in info):
+    elif any(non_pr in info for non_pr in [NON_PRIORITAIRE, NON_PRIORITAIRE_2, NON_PRIORITAIRE_3, NON_PRIORITAIRE_4]):
         return 'False'
-    elif (NON_TROUVE in info) | (NON_TROUVE_2 in info):
+    elif any(non_tr in info for non_tr in[NON_TROUVE, NON_TROUVE_2, NON_TROUVE_3, NON_TROUVE_4]):
         return 'Non trouvé'
     else:
+        with open(f'info-{nom_commune}', 'w') as file:
+            file.write(info)
         print('autre chose pour', nom_commune)
-        # print(adresse_info['fullResponseTpl'])
         return 'Presque...'
 
 def main():
@@ -54,7 +61,7 @@ def main():
     num_non_trouvées = (df['is_in_quartier_prioritaire'] == 'Non trouvé').sum()
     print(f'Nombre adresses non trouvées: {num_non_trouvées}')
 
-    df.to_excel('response.xlsx', index=False)
+    df.to_excel(FILE_NAME_ANSWER, index=False)
 
 if __name__ == '__main__':
     main()
